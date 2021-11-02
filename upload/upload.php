a2b4a0901802cc3851eddf60bd62412d3fdf3206
Cierra Rubylee Oliveira <colivei@webapp1.computing.clemson.edu>
2021-11-02 10:21:15 -0400

    testing form

---
+++ upload/upload.php
@@ -0,0 +1,15 @@
+<?php
+
+$title = $_POST['title'];
+echo title;
+
+
+// connect to mysql
+$con = mysqli_connect("mysql1.cs.clemson.edu", 
+                        "CPSC4620MTb_8b5n", 
+                        "cpsc4620-metube", 
+                        "CPSC4620-MeTube_uk7");
+
+
+
+?>
