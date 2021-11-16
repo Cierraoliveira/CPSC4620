448251e9ea4a564cc067f49439bf17b3d96ad106
Daniel Williford Ball <dwball@webapp1.computing.clemson.edu>
2021-11-16 14:27:36 -0500

    Added subscribing to channels
    - Users can now press the "subscribe" button on channel view if logged in
    - Should prevent duplicate subscriptions now

--- root/channelView.php
+++ root/channelView.php
@@ -4,6 +4,7 @@
     
     $user_id = "";
     $user_uploads = "";
+    $is_subscribed = false;
 
     if (isset($_GET)) {
         if (isset($_GET["id"])) {
@@ -47,7 +48,32 @@
             } else {
                 $user_id = "";
             }
+            $stmt = $mysqli->prepare("SELECT Sub_ID FROM Subscriptions WHERE User_ID=?") 
+                or die("Error: ".$mysqli->error);
+            $stmt->bind_param("s", $signed_in_user_id);
+            $stmt->execute();
+            $res = $stmt->get_result();
+            $subs = $res->fetch_all(MYSQLI_ASSOC);
+            foreach ($subs as $id) {
+                if ($id["Sub_ID"] == $user_id) {
+                    $is_subscribed = true;
+                    break;
+                }
+            }
             $mysqli->close();
+        }
+        if (isset($_POST["subscribe"]) && ($_POST["subscribe"] == false)) {
+            $mysqli = new mysqli(
+                "mysql1.cs.clemson.edu", 
+                "CPSC4620MTb_8b5n", 
+                "cpsc4620-metube", 
+                "CPSC4620-MeTube_uk72"
+            );
+            $stmt = $mysqli -> prepare("INSERT INTO Subscriptions VALUES(?,?)") or die("Error: ".$mysqli->error);
+            $stmt -> bind_param('ss',$signed_in_user_id, $user_id);
+            $stmt -> execute();
+            header("Location: ".$_SERVER["PHP_SELF"]."?id=$user_id");
+            die();
         }
     }
 ?>
